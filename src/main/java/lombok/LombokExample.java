d5ac413b50de0db22b0f1570575144530c87ad0f
annasimonyan1407 <annasimonyan1407@gmail.com>
2022-12-16 08:52:49 +0400

    Lombok

---
+++ src/main/java/lombok/LombokExample.java
@@ -0,0 +1,11 @@
+package lombok;
+
+public class LombokExample {
+    public static void main(String[] args) {
+        Person person = new Person();
+        person.setAge(18);
+        System.out.println(person.getAge());
+    }
+
+
+}
---
+++ src/main/java/lombok/Person.java
@@ -0,0 +1,11 @@
+package lombok;
+
+@Getter
+@Setter
+public class Person {
+    private String Name;
+    private String LastName;
+    private  int age;
+}
+
+
